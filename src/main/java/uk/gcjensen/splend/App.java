da7b121dfb508dbb6bb8e32c366c2b3b575fe748
George Jensen <george@gcjensen.uk>
2021-04-25 19:29:47 +0100

    Add initial maven project skeleton

---
+++ src/main/java/uk/gcjensen/splend/App.java
@@ -0,0 +1,11 @@
+package uk.gcjensen.splend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App  {
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+    }
+}
